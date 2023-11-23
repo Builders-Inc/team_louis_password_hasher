ec4432a554d9d3570ea54d12cd4dd56696e26900
Adedoyin-26 <ibraheem.adeleye@decagon.dev>
2023-11-23 22:32:32 +0100

    updated

---
+++ src/main/java/com/tekteam/hash/services/HashService.java
@@ -0,0 +1,5 @@
+package com.tekteam.hash.services;
+
+public interface HashService {
+
+}
---
+++ src/main/java/com/tekteam/hash/services/Serviceimplementations/HashServiceImpl.java
@@ -0,0 +1,4 @@
+package com.tekteam.hash.services.Serviceimplementations;
+
+public class HashServiceImpl {
+}
